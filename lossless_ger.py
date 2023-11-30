b9764cc6c8419fc4ba18bf6167945a6e760cc9b6
Dar9586 <atoppi2012@gmail.com>
2023-11-30 10:33:21 +0100

    Updated lossless_ger.py

--- lossless_ger.py
+++ lossless_ger.py
@@ -2,27 +2,24 @@ import numpy
 import numpy as np
 import struct
 
+a = np.array([[1., 2., 3.], [4., 5., 6.]])
 
-a = np.array([[1.,2.,3.],[4.,5.,6.]])
-#%%
-def func(val, n_bits):
-    val_real, val_imag = struct.unpack("LL", struct.pack("dd",val.real, val.imag))
-    arr = [False] * n_bits
 
-    for i in range(n_bits // 2):
-        arr[i] = val_real & (1 << (n_bits-i-1)) != 0
-    for i in range(n_bits // 2):
-        arr[i + (n_bits //2)] = val_imag & (1 << (n_bits-i-1)) != 0
+# %%
+def func(val, n_bits_component):
+    val_real, val_imag = struct.unpack("LL", struct.pack("dd", val.real, val.imag))
+    arr = [False] * (n_bits_component*2)
 
-    """import sys
-    print(arr)
-    print(sys.getsizeof(val_real))
-    print(bin(val_real))
-    sys.exit()"""
+    for i in range(n_bits_component):
+        arr[i] = val_real & (1 << (n_bits_component - i - 1)) != 0
+    for i in range(n_bits_component):
+        arr[i + n_bits_component] = val_imag & (1 << (n_bits_component - i - 1)) != 0
     return arr
 
+
 def func2(val):
-    return func(val,64)
+    return func(val, 64)
+
 
 def func3(arr):
     sh = arr.shape
@@ -32,20 +29,18 @@ def func3(arr):
         arr2.extend(func2(x))
 
     arr2 = numpy.array(arr2)
-    return arr2.reshape((*sh[:-1],-1))
+    return arr2.reshape((*sh[:-1], -1))
+
 
 def func4(arr):
     sh = arr.shape
     arr = np.transpose(arr)
-    temp = arr.reshape((-1,8))
+    temp = arr.reshape((-1, 8))
     temp2 = np.packbits(temp)
-    #x = [struct.pack("B", x) for x in temp2]
+    # x = [struct.pack("B", x) for x in temp2]
     return temp2.tobytes()
+
 
 def func6(a):
     test = func3(a)
     return func4(test)
-
-
-
-
--- zip_ger_compression.py
+++ zip_ger_compression.py
@@ -6,42 +6,18 @@ import lossless_ger
 
 from HoloUtils import getComplex, sizeof_fmt, rate
 
-dict_name = 'zip_ger_compression/'
-
 
 def zip_ger_compression(holo, filename):
-    print('ZIP_GER COMPRESSION ALGORITHM')
-    if not os.path.isdir(dict_name + filename):
-        os.makedirs(dict_name + filename)
-    np.savez(dict_name + filename + '/matrix_HOLO', holo)
-
     matrix = lossless_ger.func6(holo)
     compressed_data = zlib.compress(matrix)
-
-    with open(dict_name + filename + '/compresso.bin', 'wb') as f:
+    with open(filename, 'wb') as f:
         f.write(compressed_data)
 
-    # with open(dict_name + filename + '/matrix_dopotr.bin', 'wb') as f:
-    #    f.write(matrix)
 
 def zip_ger_decompression(filename):
-    print('ZIP_GER DECOMPRESSION ALGORITHM')
-    with open(dict_name + filename + '/compresso.bin', 'rb') as f:
+    with open(filename, 'rb') as f:
         compressed_data = f.read()
         data = zlib.decompress(compressed_data)
-
-        with open(dict_name + filename + '/decompresso.bin', 'wb') as f:
-            f.write(data)
-
-    total_size_HOL_NC = os.path.getsize(dict_name + filename + '/matrix_HOLO.npz')
-    _ , total_size_HOL_P_formatted = sizeof_fmt(total_size_HOL_NC)
-    print('NON COMPRESSA: ', total_size_HOL_P_formatted)
-
-    total_size_HOL_C = os.path.getsize(dict_name + filename + '/compresso.bin')
-    _ , total_size_HOL_P_formatted = sizeof_fmt(total_size_HOL_C)
-    print('COMPRESSA: ', total_size_HOL_P_formatted)
-
-    rate(total_size_HOL_NC, total_size_HOL_C)
 
 
 
