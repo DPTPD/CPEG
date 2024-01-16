42d30a132be19d5d9c04a563beb3e4a22044e6a9
Dar9586 <atoppi2012@gmail.com>
2024-01-16 15:32:39 +0100

    Readded precision from fpzip Changed fpzip library

--- methods/lossless/fp_algorithms/fpzip.py
+++ methods/lossless/fp_algorithms/fpzip.py
@@ -10,14 +10,14 @@ from methods.lossless.fp_algorithms.fp_algorithm import FpAlgorithm
 
 class FpzipCompressor(FpAlgorithm):
     def is_lossless(self) -> bool:
-        return True
+        return self.precision == 0
 
     def __init__(self, floatifier: Floatifier | typing.Literal["in_place", "hstack", "vstack"],
-                 g17: G17Transformer | typing.Literal[None, "bytes"]):
-        super().__init__(floatifier, g17, 0)
+                 g17: G17Transformer | typing.Literal[None, "bytes"], precision: int = 16):
+        super().__init__(floatifier, g17, precision)
 
     def _compress_float_matrix(self, matrix: np.ndarray) -> bytes:
-        return fpzip.compress(matrix)
+        return fpzip.compress(matrix, precision=self.precision)
 
     def _decompress_float_matrix(self, data: bytes) -> np.ndarray:
         matrix = fpzip.decompress(data)
