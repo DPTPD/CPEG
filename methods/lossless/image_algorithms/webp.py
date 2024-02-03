7eb79f90a194dfcc3e3b238c09f68607dcc82568
Dar9586 <atoppi2012@gmail.com>
2024-02-03 16:20:32 +0100

    webp image viewer added

--- methods/lossless/image_algorithms/webp.py
+++ methods/lossless/image_algorithms/webp.py
@@ -15,6 +15,8 @@ from methods.lossless.image_algorithms.image_compressor import ImageCompressor
 
 class WebpCompressor(ImageCompressor):
     def decompress_image(self, input_path: str) -> (np.ndarray, float, float, float, str, int):
+        if self.as_image:
+            raise NotImplementedError("Only to test purpose")
         with open(input_path, "rb") as fp:
             pp, wlen, dist, mat_dtype, webp_image, dtype_size = pickle.load(fp)
         with tempfile.NamedTemporaryFile("wb", suffix=".webp") as fp:
@@ -41,6 +43,8 @@ class WebpCompressor(ImageCompressor):
         args.append(output_path)
         subprocess.run(args, stderr=subprocess.DEVNULL)
         os.remove(png_temp_name)
+        if self.as_image:
+            return
         with open(output_path, "rb") as fp:
             data = fp.read()
         with open(output_path, "wb") as fp:
@@ -50,8 +54,10 @@ class WebpCompressor(ImageCompressor):
         return self.quality > 100
 
     def __init__(self, floatifier: Floatifier | typing.Literal["in_place", "hstack", "vstack"],
-                 g17: G17Transformer | typing.Literal[None, "bits", "bytes"], grayscale: bool = False, quality=101):
+                 g17: G17Transformer | typing.Literal[None, "bits", "bytes"], grayscale: bool = False, quality=101,
+                 as_image: bool = False):
         super().__init__(floatifier, g17)
         self.png = PngCompressor(floatifier, g17, grayscale)
         self.format_name = "webp"
         self.quality = quality
+        self.as_image = as_image
